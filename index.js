be6374c59c9ccb55b7e23362bc8da8559e14490e
LPedroSF <59021127+LPedroSF@users.noreply.github.com>
2022-11-05 23:11:43 +0000

    send

---
+++ index.js
@@ -0,0 +1,11 @@
+function openMenu(){
+    document.body.classList+="menu--open"
+}
+
+function closeMenu(){
+    document.body.classList.remove("menu--open")
+}
+
+function inProgress() {
+    alert("Whoops! This function is still not ready! Check back soon!")
+}
